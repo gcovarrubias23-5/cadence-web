2a705e85a33e46019fb8cbc3f09511d0d37c7036
gcovarrubias23-5 <gcovarrubias23@gmail.com>
2026-09-22 14:55:43 -0700

    Add overnight oats and other Cadence prebuilt plates

---
+++ src/catalogAdd.js
@@ -0,0 +1,3 @@
+export const EXTRA_BREAKFASTS = ['overnightOats', 'chiaBowl', 'skyrMango', 'proteinBagelEgg']
+export const EXTRA_LUNCHES = ['rotisserieBowl', 'whippedCottage', 'ahiBowl', 'edamameRice']
+export const EXTRA_DINNERS = ['tofuQuinoa', 'chickenSausageSheet', 'bisonFarro']
