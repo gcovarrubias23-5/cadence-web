0e6e12b6674e947bbef41c4feff4da06152c0db6
gcovarrubias23-5 <gcovarrubias23@gmail.com>
2026-09-22 15:06:50 -0700

    Map catalog food names to pantry for macro math

---
+++ src/foodAlias.js
@@ -0,0 +1,8 @@
+export const FOOD_ALIAS = {
+  'Chicken breast': 'Cooked chicken breast',
+  Broccoli: 'Broccoli, cooked',
+  'Green beans': 'Green beans, cooked',
+  Carrots: 'Carrots, cooked',
+  Onion: 'Onion, raw',
+  'Almond butter': 'Almond butter',
+}
