99e7684f2edf54342562ea6ed4d3ba86b6df2d9d
gcovarrubias23-5 <gcovarrubias23@gmail.com>
2026-09-23 19:41:06 -0700

    Softer weekly check-in line on welcome

---
+++ src/welcomeCopy.js
@@ -0,0 +1,2 @@
+export const WELCOME_LEAD = 'Come in. We will build a week that fits you \u2014 six plates a day, water beside them, and a grocery list you can carry into the store or use to order online.'
+export const WELCOME_WEEK = 'After seven days we ask if you had enough. Next week gets a little more, stays the same, or eases off.'
