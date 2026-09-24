c80e9409e4506a39d2c21a669826032646a22745
gcovarrubias23-5 <gcovarrubias23@gmail.com>
2026-09-23 19:44:15 -0700

    Welcome: track your water

--- src/welcomeCopy.js
+++ src/welcomeCopy.js
@@ -1,2 +1,1 @@
-export const WELCOME_LEAD = 'Come in. We will build a week that fits you \u2014 six plates a day, water beside them, and a grocery list you can carry into the store or use to order online.'
-export const WELCOME_WEEK = 'After seven days we ask if you had enough. Next week gets a little more, stays the same, or eases off.'
+export const WELCOME_LEAD = 'Come in. We will build a week that fits you \u2014 six plates a day, track your water, and a grocery list you can carry into the store or use to order online.'
