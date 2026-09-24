19d7eaf404e6e46d35f7d4d8a93abd157cbff835
gcovarrubias23-5 <gcovarrubias23@gmail.com>
2026-09-23 20:31:22 -0700

    Paste top sirloin steak and beef flank steak

--- src/shopPaste.js
+++ src/shopPaste.js
@@ -1,6 +1,6 @@
 export const STEAK_CUTS = [
-  { id: 'flank', label: 'Flank steak', search: 'flank steak' },
-  { id: 'sirloin', label: 'Sirloin steak', search: 'sirloin steak' },
+  { id: 'flank', label: 'Flank steak', search: 'beef flank steak' },
+  { id: 'sirloin', label: 'Sirloin steak', search: 'top sirloin steak' },
 ]
 
 const RENAME = [
